348b5c1f6fb3f0c23212b08d44e32e3dd39d7760
happiob <nakano@happiob.com>
2025-10-07 18:42:53 +0900

    fix: canonical URL with trailing slash

--- app/layout.js
+++ app/layout.js
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="ハピスク" />
-        <link rel="canonical" href="https://happisch.com" />
+        <link rel="canonical" href="https://happisch.com/" />
         <link rel="alternate" type="application/rss+xml" title="ハピスク RSS" href="/rss.xml" />
         <link rel="icon" href="data:," />
         <meta name="theme-color" content="transparent" />
